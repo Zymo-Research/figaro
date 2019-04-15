4e062761ee3edabf832a17ce792f0e348c728b7c
Michael Weinstein <michael.weinstein@ucla.edu>
2019-04-15 13:00:47 -0700

    Fixed log file name

--- defaults/environment.py
+++ defaults/environment.py
@@ -5,4 +5,4 @@ dataFolder = "/data"
 inputFolder = os.path.join(dataFolder, "input")
 outputFolder = os.path.join(dataFolder, "output")
 outputFileName = "trimParameters.json"
-logFile = os.path.join(outputFolder, "dada2.%s.log" %timestamp)
+logFile = os.path.join(outputFolder, "figaro.%s.log" %timestamp)
